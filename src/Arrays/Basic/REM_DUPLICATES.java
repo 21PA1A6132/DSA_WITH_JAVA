0a30c726d0c2c62dea83f6af4f5e94e2bfbcfd07
21pa1a6132 <21pa1a6132@vishnu.edu.in>
2024-12-09 22:59:31 +0530

    Arrays Commit

---
+++ src/Arrays/Basic/REM_DUPLICATES.java
@@ -0,0 +1,35 @@
+package Arrays.Basic;
+
+import java.util.Scanner;
+
+public class REM_DUPLICATES {
+
+    static int removeDuplicates(int[] arr) {
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                arr[i+1] = arr[j];
+                i++;
+
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the Array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the array elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = removeDuplicates(arr);
+        System.out.println("The array after removing duplicate elements is ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
